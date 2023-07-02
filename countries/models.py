f158e6fcb03dfa2ae4b59deb582f21a2bdb7eafe
AboHolo <sevamartynov2003@gmail.com>
2023-07-03 02:11:22 +0300

    fix: adding model`s meta db_table

--- countries/models.py
+++ countries/models.py
@@ -11,7 +11,7 @@ class Country(models.Model):
     median_age = models.FloatField(default=0)
 
     class Meta:
-        table = 'countries'
+        db_table = 'countries'
         ordering = ['name']
 
     def __str__(self):
